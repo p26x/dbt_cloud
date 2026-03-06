bbc9f8b6f9db64a93f44c4e9150cacc5cc70e7f0
Gautham Periyasamy <gautham25.8@gmail.com>
2026-03-06 13:48:36 +0000

    corrected ephemerals

--- models/gold/ephemeral/gold_bookings.sql
+++ models/gold/ephemeral/gold_bookings.sql
@@ -1,5 +1,5 @@
 {{config(materialized = 'ephemeral')}}
 
 WITH gold_booking AS(
-    SELECT BOOKING_ID,BOOKING_DATE,BOOKING_STATUS,BOOKING_CREATED_AT FROM {{ref('obt')}}
+    SELECT DISTINCT BOOKING_ID,BOOKING_DATE,BOOKING_STATUS,BOOKING_CREATED_AT FROM {{ref('obt')}}
 )SELECT * FROM gold_booking 
