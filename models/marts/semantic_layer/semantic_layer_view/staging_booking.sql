0b6a14a86d058cc029b2983af0150039a0301656
Gautham Periyasamy <gautham25.8@gmail.com>
2026-03-13 09:45:08 +0000

    .

--- models/marts/semantic_layer/semantic_layer_view/staging_booking.sql
+++ models/marts/semantic_layer/semantic_layer_view/staging_booking.sql
@@ -2,4 +2,5 @@
     materialized='view'
 ) }}
 
-select * from {{ source('staging', 'bookings') }}
+select *,extract(year from booking_date) as Y,extract(month from booking_date) as M
+from {{ source('staging', 'bookings') }} ORDER BY BOOKING_DATE DESC
